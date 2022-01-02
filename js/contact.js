d62f382b425569e403b0340df86af3ff40f1dae0
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-01-02 19:40:43 +0300

    Successful message sent alert

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,12 @@
 $("#send").click(()=>{
     if($("#name").val()== "" || $("#email").val()== "" || $("#message").val()== ""){
         alert("Please fill in all the details")
-    }else{
-        alert("test")
+    }
+    else
+    {
+        $("#name").val("")
+        $("#email").val("")
+        $("#message").val("")
+        alert("Your request has been submitted successfully")
     }
 })
