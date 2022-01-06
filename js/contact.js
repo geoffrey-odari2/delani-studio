b9a3e2b5c94a41ea4979fcc06756bf13a7da587c
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-01-06 09:10:11 +0300

    all scripts jqery scripts to run after page is fully loaded and separated biz from  ui logic

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,25 @@
-$("#send").click(()=>{
-    if($("#name").val()== "" || $("#email").val()== "" || $("#message").val()== ""){
-        alert("Please fill in all the details")
-    }
-    else
-    {
-        $("#name").val("")
-        $("#email").val("")
-        $("#message").val("")
-        alert("Your request has been submitted successfully")
-    }
-})
+//Busines logic
+
+const postDetails=data=>{
+  const listId = 'fdc81344d9';
+  const key = "605fdd28e1cb4ae0734ef550a4d89be5-us20"
+
+  let myHeaders = new Headers();
+  myHeaders.append("Authorization", key);
+  myHeaders.append("Content-Type", "application/json");
+
+
+  let raw = JSON.stringify(data);
+
+  let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw
+      
+    };
+
+  fetch("https://us20.api.mailchimp.com/3.0/lists/"+listId, requestOptions)
+      .then(response => response.json())
+      .then(result => (result.new_members.length()==0)? alert(new_members.error.error_code):alert("you have been sucessfully subscribed"))
+      .catch(error => alert('error', error));
+}
--- js/main.js
+++ js/main.js
@@ -1,17 +1,53 @@
-$(".service").click(function(){
-  $(this).children(".service-text").toggle()
-  $(this).children(".icon").toggle() 
-});
+//ui logic
+$(document).ready(function(){
 
-
-$( ".portfolio" ).hover(
-  function(){$(this).children("p").toggle()}
-  );
-
-
-  $("#mouse-click").click(function(){
-    $("body,html,document").scrollTop($("#about").offset().top);
+  $(".service").click(function(){
+    $(this).children(".service-text").toggle()
+    $(this).children(".icon").toggle() 
   });
+  
+  
+  $( ".portfolio" ).hover(
+    function(){$(this).children("p").toggle()}
+    );
+  
+  
+    $("#mouse-click").click(function(){
+      $("body,html,document").scrollTop($("#about").offset().top);
+    });
+
+
+    $("#send").click(()=>{
+      if($("#name").val()== "" || $("#email").val()== "" || $("#message").val()== ""){
+          alert("Please fill in all the details")
+      }
+      else
+      {
+  
+          let data = {
+              members: [
+                {
+                  email_address: $("#email").val(),
+                  status : "subscribed",
+                  merge_fields: {
+                    FNAME: $("#name").val(),
+                    MESSAGE: $("#message").val()
+                  }
+                }
+              ]
+            };
+  
+            alert("Thank you! Your message has been received")
+            $("#name").val("")
+            $("#email").val("")
+            $("#message").val("")
+         
+      }
+  })
+  
+  
+})
+
  
 
   
